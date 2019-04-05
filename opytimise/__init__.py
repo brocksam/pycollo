946401d3421e40fe16b84c63c233c8475fe2356d
Sam Brockie <sgb39@cam.ac.uk>
2019-04-05 14:34:30 +0100

    Initial package namespace management.

--- opytimise/__init__.py
+++ opytimise/__init__.py
@@ -1,1 +1,15 @@
+from .auxiliary_data import AuxiliaryData
+
+from .bounds import Bounds
+
+from .guess import Guess
+
+from .iteration import Iteration
+
+from .mesh import Mesh
+
 from .optimal_control_problem import OptimalControlProblem
+
+from .settings import Settings
+
+from .utils import quadrature_points, D_matricies

0bdd457104b45e333074e2ea2ef73da6febc1103
Sam Brockie <sgb39@cam.ac.uk>
2019-04-24 14:19:44 +0100

    extended to handle multiple different compilation forms, and quadrature computation moved to .

--- opytimise/utils.py
+++ opytimise/utils.py
@@ -1,81 +1,121 @@
-import numpy as np 
+import numba
+import numpy as np
+from numpy import sin, cos
 import scipy.interpolate as interpolate
 import sympy as sym
 
-def numbafy(expression, parameters=None, constants=None, use_cse=False, new_function_name='numbafy_func'):
+def numbafy(expression, parameters=None, constants=None, return_dims=None, return_flat=False, ocp_yu_qts_split=None):
+
+    if not parameters:
+        raise NotImplementedError
+
     cse = sym.cse(expression)
     
     code_parameters = ''
     code_constants = ''
+    code_N = ''
     code_cse = ''
 
     if parameters:
         code_parameters = ', '.join(f'{p}' for p in parameters)
+
+    # print('Parameters:')
+    # print(code_parameters, '\n')
     
     if constants:
         code_constants = []
         for k, v in constants.items():
             code_constants.append(f'{k} = {v}')
         code_constants = '\n    '.join(code_constants)
-        
-    if use_cse:
-        expressions = sym.cse(expression)
-        code_cse = []
-        for e in expressions[0]:
-            k, v = e
-            code_cse.append(f'{k} = {v}')
-        code_cse = '\n    '.join(code_cse)
-        code_expression = f'{expressions[1][0]}'
-    else:
-        code_expression = f'{expression}'
-        
 
-    template = f"""@jit
-def {new_function_name}({code_parameters}):
+    # print('Constants:')
+    # print(code_constants, '\n')
+
+    # print('SymPy Expression:')
+    # print(expression, '\n')
+
+    def factor_cse(expression):
+        expressions = sym.cse(expression)
+        expressions_factored = expressions[1][0]
+        cse_list = []
+        cse_str = ''
+        if expressions_factored:
+            for e in expressions[0]:
+                k, v = e
+                cse_list.append(f'{k} = {v}')
+            cse_str = '\n    '.join(cse_list)
+            
+            try:
+                iter(expressions_factored)
+            except TypeError:
+                expressions_factored = [expressions_factored]
+        else:
+            expressions_factored = expression
+        return expressions_factored, cse_str
+        
+    if return_dims == 0:
+        code_expression = f'{expression}'
+
+    elif return_dims == 1:
+        expressions_factored, code_cse = factor_cse(expression)
+        expression_string = ', '.join(f'{e}' for e in expressions_factored)
+        code_expression = f'np.array([{expression_string}])'
+
+    elif return_dims == 2:
+        expressions_factored, code_cse = factor_cse(expression)
+        parameter_set = set(parameters[:ocp_yu_qts_split])
+        expression_list = []
+        for e in expressions_factored[:ocp_yu_qts_split]:
+            if e.free_symbols.intersection(parameter_set):
+                expression_list.append(e)
+            else:
+                if not e:
+                    e_entry = f'np.zeros(_N)'
+                else:
+                    e_entry = f'{e}*np.ones(_N)'
+                if return_flat:
+                    e_entry = f'{e_entry}.flatten()'
+                expression_list.append(e_entry)
+
+        code_N = f'_N = {parameters[0]}.shape[0]'
+
+        expression_string = ', '.join(f'{e}' for e in expression_list)
+
+        if return_flat:
+            code_expression = f'np.concatenate([{expression_string}])'
+        else:
+            code_expression = f'np.array([{expression_string}])'
+
+    else:
+        msg = ("Value for `return_dims` argument must be 0, 1, or 2 only.")
+        raise ValueError(msg)
+
+    # print('Expression List:')
+    # print(expression_list, '\n')
+
+    # print('Expression String:')
+    # print(expression_string, '\n')
+
+    # print('Code Expression:')
+    # print(code_expression, '\n')
+
+    function_string = f"""def numbafied_func({code_parameters}):
     {code_constants}
+    {code_N}
     {code_cse}
     return {code_expression}"""
+
+#     function_string = f"""@numba.jit(nopython=True)
+# def numbafied_func({code_parameters}):
+#     {code_constants}
+#     {code_N}
+#     {code_cse}
+#     return {code_expression}"""
+
+    # print(function_string)
+    # print('\n\n\n')
+    
+    exec(function_string)
        
-    return template
+    return locals()['numbafied_func']
 
-
-
-order = list(range(2, 7))
-
-quadrature_points = {}
-quadrature_weights = {}
-D_matricies = {}
-
-for k in order:
-    num_interior_points = k - 1
-    coefficients = [0]*(num_interior_points)
-    coefficients.append(1)
-    legendre_polynomial = np.polynomial.legendre.Legendre(coefficients)
-    lobatto_points = legendre_polynomial.deriv().roots()
-    lobatto_points = np.insert(lobatto_points, 0, -1, axis=0)
-    lobatto_points = np.append(lobatto_points, 1)
-
-    lobatto_weights = np.array([2/(k*(k-1)*(legendre_polynomial(x)**2)) for x in lobatto_points])
-
-    basis_polynomials = []
-    basis_polynomials_derivs = []
-    for i, tau in enumerate(lobatto_points):
-        weightings = np.zeros_like(lobatto_points)
-        weightings[i] = 1
-        basis_polynomial = interpolate.lagrange(lobatto_points, weightings)
-        basis_polynomials.append(basis_polynomial)
-        basis_polynomials_derivs.append(basis_polynomial.deriv())
-
-    D_matrix = np.empty([k, k], dtype=object)
-
-    for dldtau, _ in enumerate(basis_polynomials_derivs):
-        for tau, _ in enumerate(lobatto_points):
-            D_matrix[tau, dldtau] = basis_polynomials_derivs[dldtau](lobatto_points[tau])
-
-    quadrature_points.update({k: lobatto_points})
-    quadrature_weights.update({k: lobatto_weights})
-    D_matricies.update({k: D_matrix})
-
-# print(quadrature_points)
-# print(quadrature_weights)
-# print(D_matricies)

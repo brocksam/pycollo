4f7c2108724fdf5bed0ccee7ef6e3f2073e104fd
Sam Brockie <sgb39@cam.ac.uk>
2019-05-13 16:41:29 +0100

    All error handling moved to post-definition analysis on calling . User can amend attributes without throwing errors. Accepts guess as iterables or dicts.

--- opytimise/guess.py
+++ opytimise/guess.py
@@ -2,160 +2,125 @@ import numpy as np
 
 class Guess():
 	
-	def __init__(self, *, optimal_control_problem=None, time=None, state=None, control=None, parameter=None, integral=None):
+	def __init__(self, *, optimal_control_problem=None, time=None, state=None, control=None, integral=None, parameter=None):
 
 		# Set optimal control problem
 		self._ocp = optimal_control_problem
 
-		# Number of nodes in guess
-		self._num_nodes = None
-
 		# Guess
-		self._t = time
-		self._y = state
-		self._u = control
-		self._q = integral
-		self._s = parameter
-		# self._path = path
-		# self._boundary = boundary
+		self.time = time
+		self.state = state
+		self.control = control
+		self.integral = integral
+		self.parameter = parameter
 
 	@property
 	def _x(self):
-		x = np.hstack([self._y.flatten(), self._u.flatten(), self._q, self._s])
+		t = np.array([t for t, t_needed in zip([self._t[0], self._t[-1]], self._ocp._bounds._t_needed) if t_needed])
+		x = np.hstack([self._y.flatten(), self._u.flatten(), self._q, t, self._s])
+		print(x)
 		return x
 
-	# @property
-	# def optimal_control_problem(self):
-	# 	return self._optimal_control_problem
+	@property
+	def time(self):
+		return self._t_user
 
-	# @property
-	# def time(self):
-	# 	return self._t
-
-	# @property
-	# def initial_time(self):
-	# 	return self._t[0]
-
-	# @property
-	# def final_time(self):
-	# 	return self._t[-1]
-	
-	# @property
-	# def state(self):
-	# 	return self._y
-	
-	# @property
-	# def control(self):
-	# 	return self._u
-
-	# @property
-	# def integral(self):
-	# 	return self._q
-
-	# @property
-	# def parameter(self):
-	# 	return self._s
-	
-	# @property
-	# def path(self):
-	# 	return self._path
-	
-	# @property
-	# def boundary(self):
-	# 	return self._boundary
-
-	def _format_as_np_array(self, guess, dims):
-		if guess is None:
-			if dims == 1:
-				guess_as_np_array = np.array([])
-			elif dims == 2:
-				list_of_lists = []
-				for _ in range(self._num_nodes):
-					list_of_lists.append([])
-				guess_as_np_array = np.array(list_of_lists)
+	@time.setter
+	def time(self, t):
+		self._t_user = None if t is None else np.array(t)
+		if t is None:
+			self._N = None 
 		else:
-			guess_as_np_array = np.array(guess)
-		return guess_as_np_array
+			try:
+				self._N = len(t.squeeze())
+			except AttributeError:
+				self._N = len(t)
+
+	@property
+	def initial_time(self):
+		return self._t_user[0]
+
+	@property
+	def final_time(self):
+		return self._t_user[-1]
 
 	def _guess_check(self):
 
 		def check_is_within_bounds(val, lower, upper):
 			if val < lower or val > upper:
-				msg = (".")
-				raise ValueError(msg.format())
+				msg = (f"The current guess lies outside of the bounds for the variable supplied by the user.")
+				raise ValueError(msg)
 
 		def check_shape(guess, num_vars, name):
-			try:
-				shape = guess.shape
-			except TypeError:
-				raise NotImplementedError
+			if guess.ndim == 1:
+				if guess.size != num_vars:
+					msg = (f"The number of variables in {name} is {num_vars} and the number of variables in the guess is {guess.size}.")
+					raise ValueError(msg)
+			elif guess.ndim == 2:
+				if guess.shape != (num_vars, self._N):
+					msg = (f"The number of variables in {name} is {num_vars} and the number of nodes in the guess is {self._N}.")
+					raise ValueError(msg)
 			else:
-				ndim = guess.ndim
-				if ndim == 1:
-					if guess.size != num_vars:
-						msg = ("The number of variables in {2} is {0} and the number of variables in the guess is {1}.")
-						raise ValueError(msg.format(num_vars, guess.size, name))
-				elif ndim == 2:
-					if shape != (num_vars, self._num_nodes):
-						msg = ("The number of variables in {2} is {0} and the number of nodes in the guess is {1}.")
-						raise ValueError(msg.format(num_vars, self._num_nodes, name))
-				else:
-					msg = (".")
-					raise ValueError(msg.format())
+				msg = (f".")
+				raise ValueError(msg)
+
+		def parse_guess_input(guess, syms, var_name, t_func=False):
+			if guess is None:
+				return np.array([])
+			dim_2 = self._N if t_func else 1
+			try:
+				iter(guess)
+			except TypeError:
+				guess = np.array([guess])
+			try:
+				len_guess = len(guess.keys())
+			except AttributeError:
+				guess_array = np.array(guess, dtype=float)
+				if guess_array.ndim == 1:
+					guess_array = guess_array.reshape(len(syms), dim_2)
+				if guess_array.shape != (len(syms), dim_2):
+					msg = (f"Guess for {var_name} must be passed as a two-dimensional iterable; an iterable of {len(syms)} iterables for each {var_name} variable of length {dim_2}. Currently guesses for {guess_array.shape[0]} {var_name} variables are supplied at {guess_array.shape[1]} nodes.")
+					raise ValueError(msg)
+			else:
+				if len(syms) != len_guess:
+					missing_guess = ', '.join(f'{symbol}' for symbol in set(syms).difference(set(guess.keys())))
+					msg = (f"Guess must be provided for each {var_name} variable. Please also provide guess for: {missing_guess}.")
+					raise ValueError(msg)
+				guess_array = np.empty((len(syms), dim_2), dtype=float)
+				for i_sym, symbol in enumerate(syms):
+					guess_array[i_sym, :] = guess[symbol]
+			return guess_array if t_func else guess_array.flatten()
 
 		bounds = self._ocp._bounds
 
 		# Time
-		check_is_within_bounds(self._t[0], bounds.initial_time_lower, bounds.initial_time_upper)
-		check_is_within_bounds(self._t[-1], bounds.final_time_lower, bounds.final_time_upper)
-		t_prev = self._t[0]
-		for t in self._t[1:]:
+		check_is_within_bounds(self._t_user[0], bounds._t0_l, bounds._t0_u)
+		check_is_within_bounds(self._t_user[-1], bounds._tF_l, bounds._tF_u)
+		t_prev = self._t_user[0]
+		for t in self._t_user[1:]:
 			if t <= t_prev:
-				msg = (".")
-				raise ValueError(msg.format())
-		self._num_nodes = len(self._t)
-		self._t = self._format_as_np_array(self._t, 2)
+				msg = (f"Guesses must be supplied in chronological order.")
+				raise ValueError(msg)
+		self._t = self._t_user
 
 		# State
-		if self._ocp._num_y_vars:
-			self._y = self._format_as_np_array(self._y, 2)
-			check_shape(self._y, self._ocp._num_y_vars, 'state')
-		else:
-			self._y = np.array([])
+		y_user = parse_guess_input(self.state, self._ocp._y_vars_user, 'state', t_func=True)
+		self._y = np.array([y for y, y_needed in zip(y_user, self._ocp._bounds._y_needed) if y_needed])
+		check_shape(self._y, self._ocp._num_y_vars, 'state')
 
 		# Control
-		if self._ocp._num_u_vars:
-			self._u = self._format_as_np_array(self._u, 2)
-			check_shape(self._u, self._ocp._num_u_vars, 'control')
-		else:
-			self._u = np.array([])
+		u_user = parse_guess_input(self.control, self._ocp._u_vars_user, 'control', t_func=True)
+		self._u = np.array([u for u, u_needed in zip(u_user, self._ocp._bounds._u_needed) if u_needed])
+		check_shape(self._u, self._ocp._num_u_vars, 'control')
 
 		# Integral
-		if self._ocp._num_q_vars:
-			self._q = self._format_as_np_array(self._q, 1)
-			check_shape(self._q, self._ocp._num_q_vars, 'integral')
-		else:
-			self._q = np.array([])
+		q_user = parse_guess_input(self.integral, self._ocp._q_vars_user, 'integral')
+		self._q = np.array([q for q, q_needed in zip(q_user, self._ocp._bounds._q_needed) if q_needed])
+		check_shape(self._q, self._ocp._num_q_vars, 'integral')
 
 		# Parameter
-		if self._ocp._num_s_vars:
-			self._s = self._format_as_np_array(self._s, 1)
-			check_shape(self._s, self._ocp._num_s_vars, 'parameter')
-		else:
-			self._s = np.array([])
-
-		# # Path
-		# if self.optimal_control_problem.num_path_constraints:
-		# 	self._path = self._format_as_np_array(self._path, 2)
-		# 	check_shape(self.path, self.optimal_control_problem.num_path_constraints, 'path')
-		# else:
-		# 	self._path = None
-
-		# # Boundary
-		# if self.optimal_control_problem.num_boundary_constraints:
-		# 	self._boundary = self._format_as_np_array(self._boundary, 1)
-		# 	check_shape(self.boundary, self.optimal_control_problem.num_boundary_constraints, 'event')
-		# else:
-		# 	self._boundary = None
+		s_user = parse_guess_input(self.parameter, self._ocp._s_vars_user, 'parameter')
+		self._s = np.array([s for s, s_needed in zip(s_user, self._ocp._bounds._s_needed) if s_needed])
+		check_shape(self._s, self._ocp._num_s_vars, 'parameter')
 
 		

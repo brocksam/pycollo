Fix for a bug that caused incorrect Jacobian calculation, both value and sparsity, for problems with variable time.

pycollo/iteration.py
@@ -161,7 +161,7 @@ class Iteration:
 		for i_c in range(self._ocp._num_y_vars * self._mesh._num_c_boundary_per_y):
 			for i_t in range(self._ocp._num_t_vars):
 				row_offset = i_c
-				col_offset = (self._ocp._num_y_vars + self._ocp._num_t_vars) * self._mesh._N + i_t
+				col_offset = (self._ocp._num_y_vars + self._ocp._num_t_vars) * self._mesh._N + self._ocp._num_q_vars + i_t
 				G_nonzero_row.append(row_offset)
 				G_nonzero_col.append(col_offset)
 		dzeta_dt_slice = slice(dzeta_du_slice.stop, len(G_nonzero_row))
@@ -307,30 +307,34 @@ class Iteration:
 		self._x_bnd_l, self._x_bnd_u = self._generate_x_bounds()
 		self._c_bnd_l, self._c_bnd_u = self._generate_c_bounds()
 
-		print('\n\n\n')
+		# print('\n\n\n')
 
-		print('x:')
-		x_data = np.ones(self._num_x)
-		print(x_data, '\n')
+		# print('x:')
+		# x_data = 2*np.ones(self._num_x)
+		# print(x_data, '\n')
 
-		print('J:')
-		J = self._objective_lambda(x_data)
-		print(J, '\n')
+		# print('J:')
+		# J = self._objective_lambda(x_data)
+		# print(J, '\n')
 
-		print('g:')
-		g = self._gradient_lambda(x_data)
-		print(g, '\n')
+		# print('g:')
+		# g = self._gradient_lambda(x_data)
+		# print(g, '\n')
 
-		print('c:')
-		c = self._constraint_lambda(x_data)
-		print(c, '\n')
+		# print('c:')
+		# c = self._constraint_lambda(x_data)
+		# print(c, '\n')
 
-		print('G:')
-		G = self._jacobian_lambda(x_data)
-		print(G, '\n')
+		# print('G:')
+		# G = self._jacobian_lambda(x_data)
+		# print(G, '\n')
+
+		# print('G Structure:')
+		# G_struct = self._jacobian_structure_lambda()
+		# print(G_struct, '\n')
 		
-		print('\n\n\n')
-		raise NotImplementedError
+		# print('\n\n\n')
+		# raise NotImplementedError
 
 		# Initialise the NLP problem
 		self._initialise_nlp()

pycollo/optimal_control_problem.py
@@ -340,7 +340,8 @@ class OptimalControlProblem():
 			y = np.vstack(x_tuple[ocp_y_slice])
 			dy = self._dy_lambda(*x_tuple)
 			stretch = self._t_strech_lambda(*x_tuple)
-			return (np.matmul(D, y.T) + stretch*np.matmul(A, dy.T)).flatten(order='F')
+			c_zeta = (np.matmul(D, y.T) + stretch*np.matmul(A, dy.T)).flatten(order='F')
+			return c_zeta
 
 		self._c_defect_lambda = c_defect_lambda
 
@@ -355,7 +356,8 @@ class OptimalControlProblem():
 			q = np.array(x_tuple[q_slice])
 			g = rho_lambda(*x_tuple)
 			stretch = self._t_strech_lambda(*x_tuple)
-			return q - stretch*np.matmul(g, W)
+			c_integral = q - stretch*np.matmul(g, W)
+			return c_integral
 
 		self._c_integral_lambda = c_integral_lambda
 
@@ -367,11 +369,11 @@ class OptimalControlProblem():
 		self._c_boundary_lambda = c_boundary_lambda
 
 		def c_lambda(x_tuple, x_tuple_point, ocp_y_slice, ocp_q_slice, num_c, defect_slice, path_slice, integral_slice, boundary_slice, A, D, W):
-			c = np.zeros(num_c)
-			# c[defect_slice] = self._c_defect_lambda(x_tuple, ocp_y_slice, A, D)
-			# c[path_slice] = self._c_path_lambda(x_tuple)
+			c = np.empty(num_c)
+			c[defect_slice] = self._c_defect_lambda(x_tuple, ocp_y_slice, A, D)
+			c[path_slice] = self._c_path_lambda(x_tuple)
 			c[integral_slice] = self._c_integral_lambda(x_tuple, ocp_q_slice, W)
-			# c[boundary_slice] = self._c_boundary_lambda(x_tuple_point)
+			c[boundary_slice] = self._c_boundary_lambda(x_tuple_point)
 			return c
 
 		self._c_lambda = c_lambda
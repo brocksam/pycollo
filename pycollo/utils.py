69aa95340a1fa2ccd165efd613d46df0096c1840
Sam Brockie <sgb39@cam.ac.uk>
2019-07-23 09:40:54 +0100

    Removed print statements that were there to help with debugging.

--- pycollo/utils.py
+++ pycollo/utils.py
@@ -4,22 +4,27 @@ from numbers import Number
 import numba
 import numpy as np
 from numpy import sin, cos, tan, exp, sqrt
+from ordered_set import OrderedSet
 import scipy.interpolate as interpolate
 import sympy as sym
 
-def numbafy(expression, parameters=None, constants=None, substitutions=None, return_dims=None, return_flat=False, N_arg=False, endpoint=False, ocp_num_vars=None):
+def numbafy(expression, parameters=None, constants=None, substitutions=None, return_dims=None, return_flat=False, N_arg=False, endpoint=False, hessian=None, hessian_sym_set=None, ocp_num_vars=None):
 
     def grouper(iterable, n):
         args = [iter(iterable)] * n
         return itertools.zip_longest(*args)
 
-    def factor_cse(expression):
+    def factor_cse(expression, ignore=[]):
         # print('Expression:')
         # print(expression, '\n')
-        expressions = sym.cse(expression)
+        if not expression:
+            return [], '', {}
+        expressions = sym.cse(expression, ignore=ignore)
         expressions_factored = expressions[1]
         # print('CSE Before:')
         # print(expressions, '\n')
+        # print('Factored Expressions:')
+        # print(expressions_factored, '\n')
         cse_list = []
         cse_str = ''
         condition_1 = not expressions_factored
@@ -43,23 +48,14 @@ def numbafy(expression, parameters=None, constants=None, substitutions=None, ret
             
 
         # print('Factored Expressions:')
-        # print(expressions_factored, '\n')
+        # print(dict(expressions[0]), '\n')
         # print('Common Sub-Expressions:')
         # print(cse_str, '\n')
 
-        return expressions_factored, cse_str
+        return expressions_factored, cse_str, dict(expressions[0])
 
     if not parameters:
         raise NotImplementedError
-
-    # print('Constants:')
-    # print(code_constants, '\n')
-
-    # print('Parameters:')
-    # print(code_parameters, '\n')
-
-    # print('SymPy Expression:')
-    # print(expression, '\n')
 
     cse = sym.cse(expression)
     
@@ -76,7 +72,8 @@ def numbafy(expression, parameters=None, constants=None, substitutions=None, ret
             expression_free_syms.update(e.free_symbols)
 
     if ocp_num_vars:
-        yu_qts_split = 2*ocp_num_vars[0]
+        yu_qts_continuous_split = sum(ocp_num_vars[0:2])
+        yu_qts_endpoint_split = 2*ocp_num_vars[0]
 
     # print('Expression Free Symbols:')
     # print(expression_free_syms, '\n')
@@ -111,40 +108,109 @@ def numbafy(expression, parameters=None, constants=None, substitutions=None, ret
         code_expression = f'{expression}'
 
     elif return_dims == 1:
-        expressions_factored, code_cse = factor_cse(expression)
+        expressions_factored, code_cse, _ = factor_cse(expression)
         if endpoint is True:
-            i_t0 = slice(0, yu_qts_split, 2)
-            i_tF = slice(1, yu_qts_split, 2)
+            i_t0 = slice(0, yu_qts_endpoint_split, 2)
+            i_tF = slice(1, yu_qts_endpoint_split, 2)
             y_t0_vars = parameters[i_t0]
             y_tF_vars = parameters[i_tF]
-            qts_vars = parameters[yu_qts_split:]
+            qts_vars = parameters[yu_qts_endpoint_split:]
             temp_list = [f'np.hstack(({y_t0}, np.zeros(_N-2), {y_tF}))' for y_t0, y_tF in zip(y_t0_vars, y_tF_vars)]
 
-            y_tuple = (f'np.hstack(({y_t0}, np.zeros(_N-2), {y_tF}))' for y_t0, y_tF in grouper(expressions_factored[:yu_qts_split], 2))
+            y_tuple = (f'np.hstack(({y_t0}, np.zeros(_N-2), {y_tF}))' for y_t0, y_tF in grouper(expressions_factored[:yu_qts_endpoint_split], 2))
             y_str = ', '.join(f'{e}' for e in y_tuple)
 
             u_str = ', '.join(f'np.zeros(_N)' for i in range(ocp_num_vars[1]))
 
-            qts_str = ', '.join(f'{e}' for e in expressions_factored[yu_qts_split:])
-            qts_str = f'np.array([{qts_str}])'
+            qts_str = ', '.join(f'{e}' for e in expressions_factored[yu_qts_endpoint_split:])
 
-            code_expression = f'np.concatenate([{y_str}, {u_str}, {qts_str}])'
+            qts_str = f'np.array([{qts_str}])' if qts_str is not '' else ''
+
+            str_list = []
+            for s in (y_str, u_str, qts_str):
+                if s is not '':
+                    str_list.append(s)
+
+            str_str = ', '.join(s for s in str_list)
+
+            code_expression = f'np.concatenate([{str_str}])'
         else:
             expression_string = ', '.join(f'{e}' for e in expressions_factored)
             code_expression = f'np.array([{expression_string}])'
 
     elif return_dims == 2:
-        expressions_factored, code_cse = factor_cse(expression)
-        parameter_set = set(parameters[:yu_qts_split])
+        parameter_set = set(parameters[:yu_qts_continuous_split])
+        if hessian in ('defect', 'path', 'integral', 'endpoint'):
+            expressions_factored, code_cse, factors_dict = factor_cse(expression, ignore=hessian_sym_set)
+            continuous_parameter_set = parameter_set.copy()
+            for k, v in substitutions.items():
+                if v.free_symbols.intersection(continuous_parameter_set):
+                    continuous_parameter_set.add(k)
+            for k, v in factors_dict.items():
+                if v.free_symbols.intersection(continuous_parameter_set):
+                    continuous_parameter_set.add(k)
+        else:
+            expressions_factored, code_cse, _ = factor_cse(expression)
+        
         expression_list = []
         for e in expressions_factored:
-            if e.free_symbols.intersection(parameter_set):
+            if hessian is 'defect':
+                lagrange_syms = e.free_symbols.intersection(hessian_sym_set)
+                e_lambda_factorised = sym.collect(e.expand(), lagrange_syms)
+                lagrange_sym_list = []
+                lagrange_arg_list = []
+                if len(lagrange_syms) > 1:
+                    while lagrange_syms:
+                        lagrange_sym = lagrange_syms.pop()
+                        for arg in e_lambda_factorised.args:
+                            if lagrange_sym in arg.free_symbols:
+                                lagrange_sym_list.append(lagrange_sym)
+                                lagrange_arg_list.append(arg / lagrange_sym)
+                else:
+                    lagrange_sym = lagrange_syms.pop()
+                    lagrange_sym_list.append(lagrange_sym)
+                    lagrange_arg_list.append(e_lambda_factorised / lagrange_sym)
+                defect_expression_list = []
+                for lagrange_sym, e_no_lagrange in zip(lagrange_sym_list, lagrange_arg_list):
+                    if e_no_lagrange.free_symbols.intersection(hessian_sym_set):
+                        msg = ("Factorisation failed.")
+                        raise ValueError(msg)
+                    if not e_no_lagrange.free_symbols.intersection(continuous_parameter_set):
+
+                        e_entry = f'np.outer({lagrange_sym}, ({e_no_lagrange})*np.ones(_N))'
+                    else:
+                        e_entry = f'np.outer({lagrange_sym}, ({e_no_lagrange}))'
+                    defect_expression_list.append(e_entry)
+                e_entry = ' + '.join(defect_expression_list)
+                expression_list.append(e_entry)
+            elif hessian is 'path':
+                raise NotImplementedError
+            elif hessian is 'integral':
+                lagrange_syms = e.free_symbols.intersection(hessian_sym_set)
+                if len(lagrange_syms) != 1:
+                    raise NotImplementedError
+                lagrange_sym = lagrange_syms.pop()
+                e_no_lagrange = sym.collect(e, lagrange_sym) / lagrange_sym
+                if e_no_lagrange.free_symbols.intersection(hessian_sym_set):
+                    msg = ("Factorisation failed.")
+                    raise ValueError(msg)
+                if not e_no_lagrange.free_symbols.intersection(continuous_parameter_set):
+
+                    e_entry = f'{lagrange_sym}*({e_no_lagrange})*np.ones(_N)'
+                else:
+                    e_entry = f'{lagrange_sym}*({e_no_lagrange})'
+                expression_list.append(e_entry)
+            elif hessian is 'endpoint':
+                raise NotImplementedError
+            elif hessian is 'objective':
+                raise NotImplementedError
+            elif e.free_symbols.intersection(parameter_set):
                 expression_list.append(e)
             else:
                 if not e:
                     e_entry = f'np.zeros(_N)'
                 else:
-                    e_entry = f'{e}*np.ones(_N)'
+                    e_entry = f'({e})*np.ones(_N)'
                 if return_flat:
                     e_entry = f'{e_entry}.flatten()'
                 expression_list.append(e_entry)
@@ -165,6 +231,15 @@ def numbafy(expression, parameters=None, constants=None, substitutions=None, ret
 
     # print('Expression String:')
     # print(expression_string, '\n')
+
+    # print('Constants:')
+    # print(code_constants, '\n')
+
+    # print('Parameters:')
+    # print(code_parameters, '\n')
+
+    # print('SymPy Expression:')
+    # print(expression, '\n')
 
     # print('Code Expression:')
     # print(code_expression, '\n')

67d8b9796fe321938076761eb16ef84024033658
Sam Brockie <sgb39@cam.ac.uk>
2019-09-30 16:13:20 +0100

    Make expression graph-related objects part of Pycollo namespace

--- pycollo/__init__.py
+++ pycollo/__init__.py
@@ -1,10 +1,16 @@
 from .bounds import Bounds
 
+from .expression_graph import *
+
 from .guess import Guess
 
 from .iteration import Iteration
 
 from .mesh import Mesh
+
+from .node import *
+
+from .operations import *
 
 from .optimal_control_problem import OptimalControlProblem
 

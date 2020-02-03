35ae0c5f1fcf44990e91491a2f933b75849a82fd
Sam Brockie <sgb39@cam.ac.uk>
2020-02-03 21:09:22 +0000

    Begin creation of radau quadrature generator method

--- pycollo/quadrature.py
+++ pycollo/quadrature.py
@@ -75,12 +75,13 @@ class Quadrature:
 
 	def A_index_array(self, order):
 		return self._retrive_or_generate_dict_value(self._A_index_arrays, order)
-	
-	def _lobatto_generator(self, order):
-		num_interior_points = order - 1
-		coefficients = [0]*(num_interior_points)
-		coefficients.append(1)
+
+	def _radau_generator(self, order):
+		coefficients = [0]*(order - 1)
+		coefficients.extend([1, 1])
+		denominator_coefficients = [1, 1]
 		legendre_polynomial = np.polynomial.legendre.Legendre(coefficients)
+		radau_points = legendre_polynomial.roots()
 		self._polynomials.update({order: legendre_polynomial})
 
 		lobatto_points = legendre_polynomial.deriv().roots()
@@ -124,8 +125,67 @@ class Quadrature:
 		A_matrix = self.butcher_array(order)[1:, :]
 		self._A_matrices.update({order: A_matrix})
 
-		# W_matrix = np.diagflat(self.quadrature_weight(order))
-		# self._W_matrices.update({order: W_matrix})
+		A_index_array = np.array(range(A_matrix.size), dtype=int)
+		self._A_index_arrays.update({order: A_index_array})
+
+		D_num_row, D_num_col = D_matrix.shape
+		D_rows = np.array(range(D_num_row), dtype=int)
+		D_left = D_rows * D_num_col
+		D_right = D_rows * (D_num_col + 1) + 1
+		D_index_array = np.concatenate((D_left, D_right))
+		D_index_array.sort()
+		self._D_index_arrays.update({order: D_index_array})
+	
+	def _lobatto_generator(self, order):
+		num_interior_points = order - 1
+		coefficients = [0]*(num_interior_points)
+		coefficients.append(1)
+		legendre_polynomial = np.polynomial.legendre.Legendre(coefficients)
+		self._polynomials.update({order: legendre_polynomial})
+
+		lobatto_points = legendre_polynomial.deriv().roots()
+		lobatto_points = np.insert(lobatto_points, 0, -1, axis=0)
+		lobatto_points = np.append(lobatto_points, 1)
+		self._quadrature_points.update({order: lobatto_points})
+
+		print(self._quadrature_points)
+		input()
+
+		lobatto_weights = np.array([1/(order*(order-1)*(legendre_polynomial(x)**2)) for x in lobatto_points])
+		self._quadrature_weights.update({order: lobatto_weights})
+
+		butcher_points = self.quadrature_point(order, domain=[0, 1])
+		butcher_array = np.zeros((order, order))
+		butcher_array[-1, :] = lobatto_weights
+		if order > 2:
+			A_row = (order + 1) * (order - 2)
+			A_col = order * (order - 2)
+			A = np.zeros((A_row, A_col))
+			b = np.zeros(A_row)
+			for k in range(order-2):
+				for j in range(order):
+					row = j + k*order
+					for i in range(order-2):
+						col = i + j*(order-2)
+						A[row, col] = lobatto_weights[i+1] * butcher_points[i+1]**k
+					b[row] = (lobatto_weights[j]/(k+1))*(1 - butcher_points[j]**(k+1)) - lobatto_weights[-1]*lobatto_weights[j]
+			del_row = []
+			for i, row in enumerate(A):
+				if np.count_nonzero(row) == 0:
+					del_row.append(i)
+			A = np.delete(A, del_row, axis=0)
+			b = np.delete(b, del_row, axis=0)
+			a = np.linalg.solve(A, b)
+			butcher_array[1:-1, :] = a.reshape(order-2, -1, order='F')
+		self._butcher_arrays.update({order: butcher_array})
+
+		D_left = np.ones((num_interior_points, 1), dtype=int)
+		D_right = np.diag(-1*np.ones((num_interior_points, ), dtype=int))
+		D_matrix = np.hstack([D_left, D_right])
+		self._D_matrices.update({order: D_matrix})
+
+		A_matrix = self.butcher_array(order)[1:, :]
+		self._A_matrices.update({order: A_matrix})
 
 		A_index_array = np.array(range(A_matrix.size), dtype=int)
 		self._A_index_arrays.update({order: A_index_array})

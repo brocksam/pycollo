f594fee9a203ae3badc551c0dd92366ebf59d426
Sam Brockie <sgb39@cam.ac.uk>
2020-02-03 14:40:17 +0000

    Ensure path Hessian not summed if no path constraints in OCP

--- pycollo/optimal_control_problem.py
+++ pycollo/optimal_control_problem.py
@@ -1070,7 +1070,7 @@ class OptimalControlProblem():
 			H[objective_index] += H_point_lambda(ddL_objective_dxbdxb)
 			H[defect_index] += H_defect_lambda(ddL_defect_dxdx, A, 
 				defect_sum_flag)
-			if self.number_path_constraints:
+			if self.number_path_constraints and path_index:
 				H[path_index] += H_path_lambda(ddL_path_dxdx)
 			H[integral_index] += H_integral_lambda(ddL_integral_dxdx, W, 
 				integral_sum_flag)

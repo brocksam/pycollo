0558c7a3284cc43f3f0c14c6af733773a9e0f189
Sam Brockie <sgb39@cam.ac.uk>
2020-02-13 14:22:14 +0000

    Adjust default problem settings to reflect changes to scaling settings in settings module

--- examples/pend.py
+++ examples/pend.py
@@ -9,7 +9,7 @@ import sympy.physics.mechanics as me
 
 import pycollo
 
-final_time = 3
+final_time = 8
 
 # REMEMBER TO HANDLE NO SUBSTITUTIONS
 
@@ -21,7 +21,11 @@ T0min, T0max = sym.symbols('T0min T0max')
 
 # Optimal Control Problem
 mesh = pycollo.Mesh(mesh_sections=10, mesh_section_fractions=None, mesh_collocation_points=4)
-problem = pycollo.OptimalControlProblem(state_variables=[y0, y1], control_variables=[u0], parameter_variables=[m0, p0], initial_mesh=mesh)
+problem = pycollo.OptimalControlProblem(
+	state_variables=[y0, y1], 
+	control_variables=[u0], 
+	# parameter_variables=[m0, p0], 
+	initial_mesh=mesh)
 
 # State equations
 problem.state_equations = [y1, (g*m0*p0*sym.cos(y0) + (((T0min + T0max)/2) + u0*(T0max - T0min)))/(m0*(k0**2 + p0**2))]
@@ -39,15 +43,34 @@ problem.state_endpoint_constraints = [problem.initial_state[0],
   problem.final_state[1]]
 
 # Bounds
-problem.bounds = pycollo.Bounds(optimal_control_problem=problem, initial_time=0, final_time=[1, final_time], state=[[-np.pi, np.pi], [-10, 10]], control=[[-0.5, 0.5]], integral=[0, 1000], parameter=[[0.5, 1.5], [0.5, 1.5]], state_endpoint=[[-np.pi/2, -np.pi/2], [0, 0], [np.pi/2, np.pi/2], [0, 0]])
+problem.bounds = pycollo.Bounds(optimal_control_problem=problem, 
+	initial_time=0, 
+	final_time=[final_time, final_time], 
+	state=[[-np.pi, np.pi], [-10, 10]], 
+	control=[[-0.5, 0.5]], 
+	integral=[0, 1000], 
+	# parameter=[[0.5, 1.5], [0.5, 1.5]], 
+	state_endpoint=[[-np.pi/2, -np.pi/2], [0, 0], [np.pi/2, np.pi/2], [0, 0]])
 
 # Guess
-problem.initial_guess = pycollo.Guess(optimal_control_problem=problem, time=[0, final_time], state=[[-np.pi/2, np.pi/2], [0, 0]], control=[0, 0], integral=[100], state_endpoints_override=True)
+problem.initial_guess = pycollo.Guess(optimal_control_problem=problem, 
+	time=[0, final_time], 
+	state=[[-np.pi/2, np.pi/2], [0, 0]], 
+	control=[0, 0], 
+	integral=[100],
+	# parameter=[1, 1],
+	state_endpoints_override=True)
 
 # Auxiliary data
-problem.auxiliary_data = {g: -9.81, d0: 0.5, k0: 1/12, T0min: -15, T0max: 15}
+problem.auxiliary_data = {g: -9.81, d0: 0.5, k0: 1/12, T0min: -15, T0max: 15,
+	m0: 1.0, p0: 1.0,
+	}
 
 problem.settings.scaling_method = 'automatic'
+problem.settings.derivative_level = 2
+problem.settings.max_mesh_iterations = 10
+problem.settings.display_mesh_result_graph = True
+
 
 # Solve
 problem.solve()

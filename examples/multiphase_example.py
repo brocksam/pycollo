268ea16da106f5b14d1afb7793e568618d64d165
Sam Brockie <sgb39@cam.ac.uk>
2020-02-27 21:45:06 +0000

    Change test script during development

--- examples/multiphase_example.py
+++ examples/multiphase_example.py
@@ -11,10 +11,22 @@ u1 = sym.Symbol('u1')
 s0 = sym.Symbol('s0')
 s1 = sym.Symbol('s1')
 
-problem = pycollo.OptimalControlProblem(parameter_variables=[s0, s1])
-phase_1 = problem.new_phase(state_variables=[y0, y1, y2, y3])
-phase_2 = problem.new_phase()
-phase_2.state_variables = [y0, y1, y2]
-# phase_3 = problem.new_phase(state_variables=[y0, y1])
+problem = pycollo.OptimalControlProblem(name='Multiphase example problem', 
+	parameter_variables=[s0, s1])
+
+phase_3 = pycollo.Phase(optimal_control_problem=problem)
+
+# phase_4 = problem.new_phase()
+
+# phase_1 = pycollo.Phase()
+# problem.add_phase(phase_1)
+
+# phase_2 = pycollo.Phase()
+# phase_2.optimal_control_problem = problem
+
+# try:
+# 	phase_1.optimal_control_problem = problem
+# except AttributeError:
+# 	pass
 
 print(problem.phases)

8c21891c89660d08cda3077700b0be8bd587bf2a
Sam Brockie <sgb39@cam.ac.uk>
2020-02-28 16:52:49 +0000

    Add console output for debugging during development

--- examples/multiphase_example.py
+++ examples/multiphase_example.py
@@ -14,19 +14,30 @@ s1 = sym.Symbol('s1')
 problem = pycollo.OptimalControlProblem(name='Multiphase example problem', 
 	parameter_variables=[s0, s1])
 
-# phase_3 = pycollo.Phase(optimal_control_problem=problem)
+phase_1 = pycollo.Phase(state_variables=y0, control_variables=u0)
+phase_1.state_equations = [y0*y1, s0*s1]
+phase_1.integrand_functions = [u0**2]
+problem.add_phase(phase_1)
 
-# phase_4 = problem.new_phase()
+phase_2 = pycollo.Phase()
+phase_2.optimal_control_problem = problem
 
-phase_1 = pycollo.Phase()
-# problem.add_phase(phase_1)
+phase_3 = pycollo.Phase(optimal_control_problem=problem)
 
-# phase_2 = pycollo.Phase()
-# phase_2.optimal_control_problem = problem
+phase_4 = problem.new_phase()
 
-# try:
-# 	phase_1.optimal_control_problem = problem
-# except AttributeError:
-# 	pass
-
-print(problem.phases)
+for phase in problem.phases:
+	print(phase.optimal_control_problem)
+	print(phase.phase_number)
+	print(phase.state_variables)
+	print(phase.control_variables)
+	print(phase.integral_variables)
+	print(phase.initial_time_variable)
+	print(phase.final_time_variable)
+	print(phase.initial_state_variables)
+	print(phase.final_state_variables)
+	print(phase.state_equations)
+	print(phase.path_constraints)
+	print(phase.integrand_functions)
+	print(phase.state_endpoint_constraints)
+	print('')

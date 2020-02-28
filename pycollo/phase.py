20f17083ee8b201dc930b42acf887f6159575bd3
Sam Brockie <sgb39@cam.ac.uk>
2020-02-28 16:51:23 +0000

    Refactor continuous time variables and constrants from old OCP class

--- pycollo/phase.py
+++ pycollo/phase.py
@@ -1,4 +1,12 @@
-from typing import ()
+"""Everything needed for defining phases within an optimal control problem.
+
+Classes:
+	Phase: 
+"""
+
+
+import itertools
+from typing import (Optional, )
 
 import sympy as sym
 
@@ -11,6 +19,9 @@ __all__ = ["Phase"]
 
 
 class Phase:
+	"""
+
+	"""
 
 	def __init__(self, *, 
 			optimal_control_problem=None, 
@@ -26,7 +37,7 @@ class Phase:
 			initial_mesh=None,
 			):
 
-		self._optimal_control_problem = None
+		self._ocp = None
 		self._phase_number = None
 		self._phase_suffix = "X"
 
@@ -93,10 +104,6 @@ class Phase:
 		phase-related information that has already been given to the optimal 
 		control problem.
 
-		Returns:
-			The optimal control problem with which this phase is associated if 
-			one has been set. Otherwise, None.
-
 		Raises:
 			AttributeError: If an :obj:`OptimalControlProblem` has already been 
 				associated with `self`. If a argument of any type other than 
@@ -108,11 +115,11 @@ class Phase:
 				to have protected attribute called `_phases` that is of a type 
 				that doesn't have an `append` method.
 		"""
-		return self._optimal_control_problem
+		return self._ocp
 
 	@optimal_control_problem.setter
 	def optimal_control_problem(self, ocp):
-		if self.optimal_control_problem is not None:
+		if self._ocp is not None:
 			msg = ('Optimal control problem is already set for this phase and '
 				'cannot be reset.')
 			raise AttributeError(msg)
@@ -124,32 +131,26 @@ class Phase:
 				'`Pycollo.OptimalControlProblem` object.')
 			raise error(msg)
 
-		self._optimal_control_problem = ocp
-		self._phase_number = self.optimal_control_problem.number_phases
+		self._ocp = ocp
+		self._phase_number = self._ocp.number_phases
 		self._phase_suffix = str(self.phase_number)
 
-	def _init_time_symbols(self):
+		self.state_variables = self.state_variables
+		self.integrand_functions = self.integrand_functions
 
-		self._t0_USER = sym.Symbol(f't0_P{self._phase_suffix}')
-		self._tF_USER = sym.Symbol(f'tF_P{self._phase_suffix}')
-		self._t0 = sym.Symbol(f'_t0_P{self._phase_suffix}')
-		self._tF = sym.Symbol(f'_tF_P{self._phase_suffix}')
-
-		self._STRETCH = 0.5 * (self._tF - self._t0)
-		self._SHIFT = 0.5 * (self._t0 + self._tF)
-
-		self._t_vars_user = (self._t0_USER, self._tF_USER)
-
-		all_internal_syms_new = (
-			self._t0_USER, 
-			self._tF_USER, 
-			self._t0, 
-			self._tF,
-			)
-
-		self._internal_symbol_replacement_mapping = dict(zip(self._all_internal_syms))
-
-		self._all_internal_syms = all_internal_syms_new
+		# old_state = self._y_vars_user
+		# old_control = self._u_vars_user
+		# self.state_variables = old_state
+		# self.control_variables = old_control
+		# self._y_vars_user + self._u_vars_user
+		# subs_mapping = dict(zip(
+		# 	(old_state + old_control), (self._y_vars_user + self._u_vars_user)))
+		# self.state_equations = [y_eqn.subs(subs_mapping)
+		# 	for y_eqn in self.state_equations]
+		# self.path_constraints = [c_con.subs(subs_mapping)
+		# 	for c_con in self.path_constraints]
+		# self.integrand_functions = [q_func.subs(subs_mapping)
+		# 	for q_func in self.integrand_functions]
 
 	@property
 	def phase_number(self) -> Optional[int]:
@@ -164,17 +165,27 @@ class Phase:
 		return self._phase_number
 
 	@property
-	def initial_time(self) -> sym.Symbol:
+	def initial_time_variable(self) -> sym.Symbol:
 		"""Symbol for the time at which this phase begins."""
-		return self._t0_USER
+		try:
+			return self._t0_USER
+		except AttributeError:
+			msg = ("Can't access initial time until associated with an optimal "
+				"control problem.")
+			raise AttributeError(msg)
 
 	@property
-	def final_time(self) -> sym.Symbol:
+	def final_time_variable(self) -> sym.Symbol:
 		"""Symbol for the time at which this phase begins."""
-		return self._tF_USER
+		try:
+			return self._tF_USER
+		except AttributeError:
+			msg = ("Can't access final time until associated with an optimal "
+				"control problem.")
+			raise AttributeError(msg)
 
 	@property
-	def initial_state(self) -> TupleSymsType:
+	def initial_state_variables(self) -> TupleSymsType:
 		"""Symbols for this phase's state variables at the initial time.
 
 		Raises:
@@ -182,10 +193,15 @@ class Phase:
 				been set to a not None value. See docstring for 
 				`state_variables` for details about why.
 		"""
-		return self._y_t0_user
+		try:
+			return self._y_t0_user
+		except AttributeError:
+			msg = ("Can't access initial state until associated with an optimal "
+				"control problem.")
+			raise AttributeError(msg)
 	
 	@property
-	def final_state(self) -> TupleSymsType:
+	def final_state_variables(self) -> TupleSymsType:
 		"""Symbols for this phase's state variables at the final time.
 
 		Raises:
@@ -193,7 +209,12 @@ class Phase:
 				been set to a not None value. See docstring for 
 				`state_variables` for details about why.
 		"""
-		return self._y_tF_user
+		try:
+			return self._y_tF_user
+		except AttributeError:
+			msg = ("Can't access initial state until associated with an optimal "
+				"control problem.")
+			raise AttributeError(msg)
 
 	@property
 	def state_variables(self) -> TupleSymsType:
@@ -218,19 +239,97 @@ class Phase:
 
 		# Generate the state endpoint variable symbols only if phase has number
 		if self.optimal_control_problem is not None:
+			self._t0_USER = sym.Symbol(f't0_P{self._phase_suffix}')
+			self._tF_USER = sym.Symbol(f'tF_P{self._phase_suffix}')
+			self._t0 = sym.Symbol(f'_t0_P{self._phase_suffix}')
+			self._tF = sym.Symbol(f'_tF_P{self._phase_suffix}')
+			self._STRETCH = 0.5 * (self._tF - self._t0)
+			self._SHIFT = 0.5 * (self._t0 + self._tF)
+			self._t_vars_user = (self._t0_USER, self._tF_USER)
+
 			self._y_t0_user = tuple(sym.Symbol(f'{y}_P{self._phase_suffix}(t0)')
 				for y in self._y_vars_user)
 			self._y_tF_user = tuple(sym.Symbol(f'{y}_P{self._phase_suffix}(tF)')
 				for y in self._y_vars_user)
-		# self._update_vars()
-		_ = check_sym_name_clash(self._y_vars_user)
+		
+		check_sym_name_clash(self._y_vars_user)
 
 	@property
 	def number_state_variables(self):
-		if self._bounds._bounds_checked:
-			return self._bounds._y_needed.sum()
-		else:
-			return len(self._y_vars_user)
+		return len(self._y_vars_user)
+
+	@property
+	def control_variables(self):
+		return self._u_vars_user
+
+	@control_variables.setter
+	def control_variables(self, u_vars):
+		self._u_vars_user = format_as_tuple(u_vars)
+		_ = check_sym_name_clash(self._u_vars_user)
+
+	@property
+	def number_control_variables(self):
+		return len(self._u_vars_user)
+
+	@property
+	def integral_variables(self):
+		return self._q_vars_user
+
+	@property
+	def number_integral_variables(self):
+		return len(self._q_vars_user)
+
+	@property
+	def state_equations(self):
+		return self._y_eqns_user
+
+	@state_equations.setter
+	def state_equations(self, y_eqns):
+		self._y_eqns_user = format_as_tuple(y_eqns)
+
+	@property
+	def number_state_equations(self):
+		return len(self._y_eqns_user)
+
+	@property
+	def path_constraints(self):
+		return self._c_cons_user
+
+	@path_constraints.setter
+	def path_constraints(self, c_cons):
+		self._c_cons_user = format_as_tuple(c_cons)
+
+	@property
+	def number_path_constraints(self):
+		return len(self._c_cons_user)
+
+	@property
+	def integrand_functions(self):
+		return self._q_funcs_user
+
+	@integrand_functions.setter
+	def integrand_functions(self, integrands):
+		self._q_funcs_user = format_as_tuple(integrands)
+		self._q_vars_user = tuple(sym.Symbol(f'q{i_q}_P{self._phase_suffix}') 
+			for i_q, _ in enumerate(self._q_funcs_user))
+
+	@property
+	def number_integrand_functions(self):
+		return len(self._q_funcs_user)
+
+	@property
+	def state_endpoint_constraints(self):
+		return self._y_b_cons_user
+
+	@state_endpoint_constraints.setter
+	def state_endpoint_constraints(self, y_b_cons):
+		self._is_initialised = False
+		y_b_cons = format_as_tuple(y_b_cons)
+		self._y_b_cons_user = tuple(y_b_cons)
+
+	@property
+	def number_state_endpoint_constraints(self):
+		return len(self._y_b_cons_user)
 
 	def __str__(self):
 		string = (f"Phase {self.phase_number} of {self.optimal_control_problem}")

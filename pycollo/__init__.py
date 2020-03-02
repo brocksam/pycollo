1d43cc7ccf58f5bf31483dbc74968d1b89fbb59c
Sam Brockie <sgb39@cam.ac.uk>
2020-03-02 19:41:56 +0000

    Remove package level import of all objects except OptimalControlProblem class

--- pycollo/__init__.py
+++ pycollo/__init__.py
@@ -1,13 +1,1 @@
-from .bounds import Bounds
-
-from .guess import Guess
-
-from .iteration import Iteration
-
-from .mesh import Mesh
-
-from .optimal_control_problem import OptimalControlProblem
-
-from .phase import *
-
-from .settings import Settings
+from .optimal_control_problem import *

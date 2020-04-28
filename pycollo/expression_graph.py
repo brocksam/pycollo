b8da7a3f216ef4d721bc6fbea093822ba83a3933
Sam Brockie <sgb39@cam.ac.uk>
2020-04-29 00:30:37 +0100

    Remove comments

--- pycollo/expression_graph.py
+++ pycollo/expression_graph.py
@@ -523,14 +523,7 @@ class ExpressionGraph:
 			dependent_nodes_by_tier_collapsed)
 
 		derivative = compute_derivative_recursive_hSAD_algorithm()
-
-		# try:
-		# 	for r, c in [(1, 0), (1, 1), (1, 3), (4, 0), (4, 3), (4, 4), (9, 9)]:
-		# 		print(f"({r}, {c}): {derivative.row(r).col(c)}")
-		# 	input()
-		# except IndexError:
-		# 	pass
-
+		
 		return derivative
 
 	def __str__(self):

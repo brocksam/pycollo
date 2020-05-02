72f4182a0dc3630c0d76d1f9c5c9848abc0f50fa
Sam Brockie <sgb39@cam.ac.uk>
2020-05-02 14:32:42 +0100

    Refactor Hessian numbafication to own module with dedicated Hessian numbafication functions

--- pycollo/numbafy_hessian.py
+++ pycollo/numbafy_hessian.py
@@ -1,6 +1,3 @@
-import itertools
-from numbers import Number
-
 import numba as nb
 import numpy as np
 from numpy import (sin, cos, tan, exp, log, sqrt, arccos, arcsin, arctan, tanh)
@@ -12,25 +9,10 @@ asin = arcsin
 atan = arctan
 
 
-def numbafy_objective_hessian(*args, **kwargs):
+def numbafy_endpoint_hessian(*args, **kwargs):
 
-	def grouper(iterable, n):
-		args = [iter(iterable)] * n
-		return itertools.zip_longest(*args)
-
-	def expand_to_array(e, node):
-		if e is zero_sym or e == 0:
-			e_entry = f'_np_zeros_array_N'
-		elif e is one_sym:
-			e_entry = f'_np_ones_array_N'
-		elif node.is_vector:
-			e_entry = f'{e}'
-		else:
-			e_entry = f'({e})*_np_ones_array_N'
-		return e_entry
-
-	def collect_function_arguments(parameters, sigma):
-		parameters_all = parameters + (sigma, )
+	def collect_function_arguments(parameters, sigma, lagrange_multipliers):
+		parameters_all = parameters + (sigma, ) + lagrange_multipliers
 		function_arguments = ', '.join(f'{p}' for p in parameters_all)
 		return function_arguments
 
@@ -55,41 +37,29 @@ def numbafy_objective_hessian(*args, **kwargs):
 			for sub_tier in substitution_tiers)
 		return intermediate_substitutions
 
-	def collect_return_value(expression, expression_nodes, sigma):
-		values = []
-		nonzero_row_indices = []
-		nonzero_col_indices = []
-		for i_row, expr_node_row in enumerate(expression_nodes):
-			expr_row = expression.row(i_row)
-			for i_col, (expr, expr_node) in enumerate(zip(expr_row, expr_node_row)):
-				if expr_node != expression_graph._zero_node and not np.isclose(expr, 0):
-					values.append(expr)
-					nonzero_row_indices.append(i_row)
-					nonzero_col_indices.append(i_row)
-		nonzero_indices = (nonzero_row_indices, nonzero_col_indices)
-		return_value = f"sigma * np.array([{', '.join(values)}])"
-		return return_value, nonzero_indices
+	def collect_return_value(expression):
+		nonzeros = ', '.join([str(v) for v in expression.entries.values()])
+		return_value = f"np.array([{nonzeros}])"
+		return return_value
 
-	cout_function_string = True
+	cout_function_string = False
 
 	expression_graph = kwargs.get("expression_graph")
 	expression = kwargs.get("expression")
-	expression_nodes = kwargs.get("expression_nodes")
-	expression_nodes = np.array(expression_nodes).reshape(expression.shape)
 	precomputable_nodes = kwargs.get("precomputable_nodes")
 	dependent_tiers = kwargs.get("dependent_tiers")
 	parameters = kwargs.get("parameters")
-	sigma = kwargs.get("sigma")
+	sigma = kwargs.get("objective_factor")
+	lagrange_multipliers = kwargs.get("lagrange_multipliers")
 
-	function_arguments = collect_function_arguments(parameters, sigma)
+	function_arguments = collect_function_arguments(parameters, sigma, lagrange_multipliers)
 	numpy_default_arrays = collect_numpy_default_arrays()
 	precomputed_constants = collect_precomputable_constants(precomputable_nodes)
 	intermediate_substitutions = collect_intermediate_substitutions(dependent_tiers)
-	return_value, nonzero_indices = collect_return_value(expression, expression_nodes, sigma)
+	return_value = collect_return_value(expression)
 
 	function_string = (
 		f"def numbafied_func({function_arguments}):\n"
-		f"    {numpy_default_arrays}\n"
 		f"    {precomputed_constants}\n"
 		f"    {intermediate_substitutions}\n"
 		f"    return {return_value}")
@@ -100,11 +70,102 @@ def numbafy_objective_hessian(*args, **kwargs):
 	
 	exec(function_string)
 	   
-	return locals()['numbafied_func'], nonzero_indices
+	return locals()['numbafied_func']
 
 
-def cout(*args):
-	print('\n\n')
-	for arg in args:
-		print(f'{arg}\n')
+
+
+def numbafy_continuous_hessian(*args, **kwargs):
+
+	def collect_function_arguments(parameters, lagrange_multipliers):
+		parameters_all = parameters + lagrange_multipliers
+		function_arguments = ', '.join(f'{p}' for p in parameters_all)
+		return function_arguments
+
+	def collect_numpy_default_arrays():
+		return ""
+
+	def collect_precomputable_constants(precomputable_nodes):
+		if precomputable_nodes:
+			precomputed_constants = '\n    '.join(f'{node.numbafy_expression}' 
+				for node in precomputable_nodes)
+		else:
+			precomputed_constants = '    '
+		return precomputed_constants
+
+	def collect_intermediate_substitutions(dependent_tiers, final_nodes):
+
+		def check_if_needs_summing(node):
+			if node in final_nodes:
+				new_sym = sym.Symbol(f"_sum{node.symbol}")
+				new_parent_syms = [sym.Symbol(f"{parent.symbol}") 
+					if parent in summing_nodes else parent.symbol
+					for parent in node.parent_nodes]
+				new_sum_mapping = {parent: f"np.sum({parent})" for parent in new_parent_syms}
+				new_expr = node.operation.SYMPY_OP(*[new_parent_sym 
+					for new_parent_sym in new_parent_syms])
+				sum_expr = f"{new_sym} = {new_expr}"
+				for k, v in new_sum_mapping.items():
+					start = sum_expr.find(str(k))
+					end = start + len(str(k))
+					sum_expr = sum_expr[:start] + str(v) + sum_expr[end:]
+				sum_expr = f"{new_sym} = {sum_expr}"
+				expr = f"{sum_expr}"
+				return expr
+			return f"{node.numbafy_expression}"
+
+		substitution_tiers = []
+		for tier in list(dependent_tiers.values())[1:]:
+			tier_substitutions = '\n    '.join(f"{check_if_needs_summing(node)}\n"
+				for node in tier)
+			substitution_tiers.append(tier_substitutions)
+		intermediate_substitutions = '\n    '.join(f'{sub_tier}' 
+			for sub_tier in substitution_tiers)
+
+		return intermediate_substitutions
+
+	def collect_return_value(expression, expression_nodes, final_nodes):
+
+		def check_if_sum_sym_needed(symbol, node):
+			if node in final_nodes:
+				return str(sym.Symbol(f"_sum{symbol}"))
+			else:
+				return str(symbol)
+
+		return_value = ", ".join([check_if_sum_sym_needed(symbol, node) for symbol, node in zip(expression.entries.values(), expression_nodes.entries.values())])
+		return return_value
+
+	cout_function_string = False
+
+	expression_graph = kwargs.get("expression_graph")
+	expression = kwargs.get("expression")
+	expression_nodes = kwargs.get("expression_nodes")
+	precomputable_nodes = kwargs.get("precomputable_nodes")
+	dependent_tiers = kwargs.get("dependent_tiers")
+	parameters = kwargs.get("parameters")
+	sigma = kwargs.get("objective_factor")
+	lagrange_multipliers = kwargs.get("lagrange_multipliers")
+	summing_nodes = kwargs.get("summing_nodes")
+
+	final_nodes = set(expression_nodes.entries.values()).intersection(summing_nodes)
+
+	function_arguments = collect_function_arguments(parameters, lagrange_multipliers)
+	precomputed_constants = collect_precomputable_constants(precomputable_nodes)
+	intermediate_substitutions = collect_intermediate_substitutions(dependent_tiers, final_nodes)
+	return_value = collect_return_value(expression, expression_nodes, final_nodes)
+
+	function_string = (
+		f"def numbafied_func({function_arguments}):\n"
+		f"    {precomputed_constants}\n"
+		f"    {intermediate_substitutions}\n"
+		f"    return {return_value}")
+
+	if cout_function_string:
+		cout(function_string)
+		input()
+	
+	exec(function_string)
+	   
+	return locals()['numbafied_func']
+
 

df0c171c00bc57078da7cd84ddd69759b7cfccaf
Sam Brockie <sgb39@cam.ac.uk>
2020-09-05 23:38:44 +0100

    Convert scaling and output properties to processed properties

--- pycollo/settings.py
+++ pycollo/settings.py
@@ -7,24 +7,44 @@ NLP solvers etc.
 Attributes
 ----------
 BACKEND_CASADI_KEYWORD : str
-    Description
+    Constant keyword string identifier for the Pycollo-CasADi backend.
 BACKEND_PYCOLLO_KEYWORD : str
-    Description
-DEFAULT_BACKEND : TYPE
-    Description
+    Constant keyword string identifier for the Pycollo-only (hSAD) backend.
+BACKEND_SYMPY_KEYWORD : str
+    Constant keyword string identifier for the Pycollo-Sympy backend.
+DEFAULT_BACKEND : str
+    The default backend to be used (via its constant keyword string
+    identifier).
 DEFAULT_COLLOCATION_POINTS_MAX : int
-    Description
+    Constant default limitation on the maximum number of collocation points
+    per mesh section that a user can specify. The value of 20 has been chosen
+    as above this the algorithms that are used for evaluating the orthogonal
+    polynomials become numerically unstable and raise a warning.
 DEFAULT_COLLOCATION_POINTS_MIN : int
-    Description
-DEFAULT_DERIVATIVE_LEVEL : TYPE
-    Description
+    Constant default limitation on the minimum number of collocation points
+    per mesh section that a user can specify. The value of 2 has been chosen
+    as this is the smallest possible value that still makes logical sense (i.e.
+    a mesh section cannot have fewer than two nodes).
+DEFAULT_DERIVATIVE_LEVEL : int
+    Default derivative level to be used by the Pycollo-backends and NLP solvers
+    with 1 and 2 corresponding to first- and second-order mode respectively.
+    By default second-order mode is chosen (via its constant identifier) as it
+    generally gives superior performance.
 DEFAULT_DISPLAY_MESH_REFINE_INFO : bool
-    Description
+    Default value of whether mesh refinement information should be outputted
+    to the console at the end of each mesh iteration. Value is set to True as
+    this information is generally interesting/useful to users.
 DEFAULT_DISPLAY_MESH_RESULT_GRAPH : bool
-    Description
+    Default value of whether graphs should be plotting displaying the state,
+    state derivatives and control variables at the end of each mesh iteration. 
+    Value is set to False as displaying these causes the NLP solver to pause 
+    between sucessive mesh iterations.
 DEFAULT_DISPLAY_MESH_RESULT_INFO : bool
-    Description
-DEFAULT_LINEAR_SOLVER : TYPE
+    Default value of whether information about the mesh result should be
+    outputted to the console at the end of each mesh iteration. Value is set to
+    False as mesh results tend to be large streams of data that are difficult
+    to interpret.
+DEFAULT_LINEAR_SOLVER : str
     Description
 DEFAULT_MATRIX_FORM : TYPE
     Description
@@ -64,6 +84,8 @@ NLP_SOLVER_BONMIN_KEYWORD : str
     Description
 NLP_SOLVER_IPOPT_KEYWORD : str
     Description
+NLP_SOLVER_SNOPT_KEYWORD : str
+    Description
 NLP_SOLVER_WORHP_KEYWORD : str
     Description
 QUADRATURE_GAUSS_KEYWORD : str
@@ -76,9 +98,23 @@ SCALING_BOUNDS_KEYWORD : str
     Description
 SCALING_GUESS_KEYWORD : str
     Description
+SCALING_NONE_IDENTIFIER : TYPE
+    Description
 SCALING_NONE_KEYWORD : TYPE
     Description
 SCALING_USER_KEYWORD : str
+    Description
+UNSUPPORTED_BACKEND : TYPE
+    Description
+UNSUPPORTED_COLLOCATION_MATRIX_FORM : TYPE
+    Description
+UNSUPPORTED_LINEAR_SOLVER : tuple
+    Description
+UNSUPPORTED_NLP_SOLVER : TYPE
+    Description
+UNSUPPORTED_QUADRATURE_METHOD : TYPE
+    Description
+UNSUPPORTED_SCALING_METHOD : TYPE
     Description
 
 """
@@ -99,13 +135,18 @@ UNSUPPORTED_BACKEND = (BACKEND_SYMPY_KEYWORD, )
 MATRIX_FORM_DIFFERENTIAL_KEYWORD = "differential"
 MATRIX_FORM_INTEGRAL_KEYWORD = "integral"
 DEFAULT_MATRIX_FORM = MATRIX_FORM_INTEGRAL_KEYWORD
+UNSUPPORTED_COLLOCATION_MATRIX_FORM = (MATRIX_FORM_DIFFERENTIAL_KEYWORD, )
 
 # Scaling method constants
-SCALING_NONE_KEYWORD = None
+SCALING_NONE_IDENTIFIER = None
+SCALING_NONE_KEYWORD = "none"
 SCALING_USER_KEYWORD = "user"
 SCALING_GUESS_KEYWORD = "guess"
 SCALING_BOUNDS_KEYWORD = "bounds"
 DEFAULT_SCALING_METHOD = SCALING_BOUNDS_KEYWORD
+UNSUPPORTED_SCALING_METHOD = (SCALING_USER_KEYWORD,
+                              SCALING_GUESS_KEYWORD,
+                              )
 DEFAULT_UPDATE_SCALING = True
 DEFAULT_NUMBER_SCALING_SAMPLES = 0
 DEFAULT_SCALING_WEIGHT = 0.8
@@ -132,6 +173,7 @@ QUADRATURE_GAUSS_KEYWORD = "gauss"
 QUADRATURE_LOBATTO_KEYWORD = "lobatto"
 QUADRATURE_RADAU_KEYWORD = "radau"
 DEFAULT_QUADRATURE_METHOD = QUADRATURE_LOBATTO_KEYWORD
+UNSUPPORTED_QUADRATURE_METHOD = (QUADRATURE_GAUSS_KEYWORD, )
 DEFAULT_COLLOCATION_POINTS_MIN = 4
 DEFAULT_COLLOCATION_POINTS_MAX = 10
 
@@ -153,9 +195,13 @@ DEFAULT_DISPLAY_MESH_RESULT_GRAPH = False
 class Settings():
 
     """Settings class for all Pycollo OCP and NLP settings.
-
+    
     Attributes
     ----------
+    backend : TYPE
+        Description
+    collocation_matrix_form : TYPE
+        Description
     collocation_points_max : int
         Minimum allowable number of mesh points per mesh section.
     collocation_points_min : int
@@ -195,18 +241,20 @@ class Settings():
         Description
     update_scaling : bool
         Description
-
+    
     """
 
     _BACKEND_OPTIONS = (
         BACKEND_PYCOLLO_KEYWORD,
         BACKEND_CASADI_KEYWORD,
+        BACKEND_SYMPY_KEYWORD,
     )
     _COLLOCATION_MATRIX_FORMS = (
         MATRIX_FORM_DIFFERENTIAL_KEYWORD,
         MATRIX_FORM_INTEGRAL_KEYWORD,
     )
-    _SCALING_OPTIONS = (
+    _SCALING_METHOD_OPTIONS = (
+        SCALING_NONE_IDENTIFIER,
         SCALING_NONE_KEYWORD,
         SCALING_USER_KEYWORD,
         SCALING_GUESS_KEYWORD,
@@ -222,7 +270,7 @@ class Settings():
         LINEAR_SOLVER_MUMPS_KEYWORD,
         LINEAR_SOLVER_MA57_KEYWORD,
     )
-    _QUADRATURE_METHODS = (
+    _QUADRATURE_METHOD_OPTIONS = (
         QUADRATURE_GAUSS_KEYWORD,
         QUADRATURE_LOBATTO_KEYWORD,
         QUADRATURE_RADAU_KEYWORD,
@@ -245,6 +293,22 @@ class Settings():
         type=int,
         cast=True,
         options=_DERIVATIVE_LEVELS,
+    )
+    collocation_matrix_form = processed_property(
+        "collocation_matrix_form",
+        description="form of the collocation matrices",
+        type=str,
+        cast=True,
+        options=_COLLOCATION_MATRIX_FORMS,
+        unsupported_options=UNSUPPORTED_COLLOCATION_MATRIX_FORM,
+    )
+    quadrature_method = processed_property(
+        "quadrature_method",
+        description="quadrature method",
+        type=str,
+        cast=True,
+        options=_QUADRATURE_METHOD_OPTIONS,
+        unsupported_options=UNSUPPORTED_QUADRATURE_METHOD,
     )
     nlp_solver = processed_property(
         "nlp_solver",
@@ -274,6 +338,84 @@ class Settings():
         type=int,
         cast=True,
         min=1,
+    )
+    collocation_points_min = processed_property(
+        "collocation_points_min",
+        description="minimum number of collocation points per mesh section",
+        type=int,
+        cast=True,
+        min=2,
+        max=20,
+        at_most="collocation_points_max",
+    )
+    collocation_points_max = processed_property(
+        "collocation_points_max",
+        description="maximum number of collocation points per mesh section",
+        type=int,
+        cast=True,
+        min=2,
+        max=20,
+        at_least="collocation_points_min"
+    )
+    mesh_tolerance = processed_property(
+        "mesh_tolerance",
+        description="mesh tolerance",
+        type=float,
+        max=1.0,
+        min=0.0,
+        exclusive=True,
+    )
+    max_mesh_iterations = processed_property(
+        "max_mesh_iterations",
+        description="maximum number of mesh iterations",
+        type=int,
+        cast=True,
+        min=1,
+    )
+    scaling_method = processed_property(
+        "scaling_method",
+        description="scaling method",
+        options=_SCALING_METHOD_OPTIONS,
+        unsupported_options=UNSUPPORTED_SCALING_METHOD,
+    )
+    update_scaling = processed_property(
+        "update_scaling",
+        description="update scaling factors after each mesh iteration",
+        type=bool,
+        cast=True,
+    )
+    number_scaling_samples = processed_property(
+        "number_scaling_samples",
+        description="number of samples taken when computing scaling factors",
+        type=int,
+        cast=True,
+        min=0,
+    )
+    scaling_weight = processed_property(
+        "scaling_weight",
+        description="inter-mesh scaling adjustment weighting factor",
+        type=float,
+        max=1.0,
+        min=0.0,
+        exclusive=True,
+    )
+    display_mesh_refinement_info = processed_property(
+        "display_mesh_refinement_info",
+        description="display the mesh refinement information",
+        type=bool,
+        cast=True,
+    )
+    display_mesh_result_info = processed_property(
+        "display_mesh_result_info",
+        description="display the mesh result information",
+        type=bool,
+        cast=True,
+    )
+    display_mesh_result_graph = processed_property(
+        "display_mesh_result_graph",
+        description="display a graph of result after each mesh iteration",
+        type=bool,
+        cast=True,
     )
 
     def __init__(self, *,
@@ -311,192 +453,36 @@ class Settings():
         self.nlp_tolerance = nlp_tolerance
         self.max_nlp_iterations = max_nlp_iterations
 
-        # # Collocation and quadrature
-        # self.collocation_matrix_form = collocation_matrix_form
-        # self.quadrature_method = quadrature_method
+        # Collocation and quadrature
+        self.collocation_matrix_form = collocation_matrix_form
+        self.quadrature_method = quadrature_method
         self.derivative_level = derivative_level
 
-        # # Mesh refinement
-        # self.collocation_points_min = collocation_points_min
-        # self.collocation_points_max = collocation_points_max
-        # self.mesh_tolerance = mesh_tolerance
-        # self.max_mesh_iterations = max_mesh_iterations
+        # Mesh refinement
+        self.collocation_points_min = collocation_points_min
+        self.collocation_points_max = collocation_points_max
+        self.mesh_tolerance = mesh_tolerance
+        self.max_mesh_iterations = max_mesh_iterations
 
-        # # Scaling
-        # self.scaling_method = scaling_method
-        # self.update_scaling = update_scaling
-        # self.number_scaling_samples = number_scaling_samples
-        # self.scaling_weight = scaling_weight
+        # Scaling
+        self.scaling_method = scaling_method
+        self.update_scaling = update_scaling
+        self.number_scaling_samples = number_scaling_samples
+        self.scaling_weight = scaling_weight
 
         # # Output information
-        # self.display_mesh_refinement_info = display_mesh_refinement_info
-        # self.display_mesh_result_info = display_mesh_result_info
-        # self.display_mesh_result_graph = display_mesh_result_graph
+        self.display_mesh_refinement_info = display_mesh_refinement_info
+        self.display_mesh_result_info = display_mesh_result_info
+        self.display_mesh_result_graph = display_mesh_result_graph
 
     @property
     def optimal_control_problem(self):
+        """User-friendly alias to the optimal control problem attribute.
+
+        Returns
+        -------
+        :obj:`OptimalControlProblem`
+            The Pycollo optimal control problem object to which these settings
+            are to be associated with.
+        """
         return self.ocp
-
-    @property
-    def scaling_method(self) -> Optional[str]:
-        return self._scaling_method
-
-    @scaling_method.setter
-    def scaling_method(self, method: Optional[str]):
-        if method is not None:
-            method = method.casefold()
-        if method not in self._SCALING_OPTIONS:
-            msg = (f"{method} is not a valid scaling option.")
-            raise ValueError(msg)
-        self._scaling_method = method
-
-    @property
-    def update_scaling(self):
-        return self._update_scaling
-
-    @update_scaling.setter
-    def update_scaling(self, do_update):
-        do_update = bool(do_update)
-        self._update_scaling = do_update
-
-    @property
-    def scaling_weight(self):
-        return self._scaling_weight
-
-    @scaling_weight.setter
-    def scaling_weight(self, weight):
-        weight = float(weight)
-        if weight < 0 or weight > 1:
-            msg = (f'Scaling weight must be a number between 0 and 1. {weight} '
-                    'is invalid.')
-            raise ValueError(msg)
-        self._scaling_weight = weight
-
-    @property
-    def number_scaling_samples(self):
-        return self._number_scaling_samples
-
-    @number_scaling_samples.setter
-    def number_scaling_samples(self, num):
-        num = int(num)
-        if num < 0:
-            msg = (f'Number of scaling samples must be an integer greater than '
-                    f'or equal to 0. {num} is invalid.')
-        self._number_scaling_samples = num
-
-    @property
-    def collocation_matrix_form(self):
-        return self._col_mat_form
-
-    @collocation_matrix_form.setter
-    def collocation_matrix_form(self, form):
-        form = form.casefold()
-        if form not in self._COLLOCATION_MATRIX_FORMS:
-            msg = (f"{form} is not a valid collocation matrix form.")
-            raise ValueError(msg)
-        elif form == 'derivative':
-            msg = ("Derivative matrix form is not currently supported. Please "
-                   "use integral matrix form.")
-            raise ValueError(msg)
-        self._col_mat_form = form
-
-    @property
-    def quadrature_method(self):
-        return self._quadrature_method
-
-    @quadrature_method.setter
-    def quadrature_method(self, method):
-        method = method.casefold()
-        if method not in self._QUADRATURE_METHODS:
-            msg = ("The quadrature method of '{}' is not a valid argument string.")
-            raise ValueError(msg.format(method))
-        self._quadrature_method = method
-
-    # @property
-    # def derivative_level(self):
-    #     return self._derivative_level
-
-    # @derivative_level.setter
-    # def derivative_level(self, deriv_level):
-    #     deriv_level = int(deriv_level)
-    #     if deriv_level not in self._DERIVATIVE_LEVELS:
-    #         msg = (f"Derivative level must be set to either 1 (which uses the gradient vector of the objective function and the Jacobian matrix of the constraints vector) or 2 (which also uses the Hessian matrix of the Lagrangian of the constraints).")
-    #         raise ValueError(msg)
-    #     self._derivative_level = deriv_level
-
-    @property
-    def mesh_tolerance(self):
-        return self._mesh_tolerance
-
-    @mesh_tolerance.setter
-    def mesh_tolerance(self, tolerance):
-        if tolerance <= 0:
-            msg = (
-                "Tolerance for the mesh must be a postive real number. {} is invalid.")
-            raise ValueError(msg.format(tolerance))
-        self._mesh_tolerance = tolerance
-
-    @property
-    def max_mesh_iterations(self):
-        return self._max_mesh_iterations
-
-    @max_mesh_iterations.setter
-    def max_mesh_iterations(self, max_mesh_iterations):
-        if max_mesh_iterations <= 0:
-            msg = (f"Maximum number of mesh iterations must be a positive integer. {max_mesh_iterations} is invalid.")
-            raise ValueError(msg)
-        self._max_mesh_iterations = int(max_mesh_iterations)
-
-    @property
-    def collocation_points_min(self):
-        return self._col_points_min
-
-    @collocation_points_min.setter
-    def collocation_points_min(self, points_min):
-        points_min = int(points_min)
-        self._col_points_min = points_min
-        if points_min < 2:
-            msg = ("The minimum number of collocation points must be great than 2.")
-            raise ValueError(msg)
-        if points_min > 4:
-            msg = ("It is recommended that a minimum of 2, 3 or 4 collocation points is used per mesh section to allow for efficient computation.")
-            raise ValueError(msg)
-
-    @property
-    def collocation_points_max(self):
-        return self._col_points_max
-
-    @collocation_points_max.setter
-    def collocation_points_max(self, points_max):
-        points_max = int(points_max)
-        if points_max < self._col_points_min:
-            msg = ("The maximum number of collocation points must be greater than or equal to {}, the minimum number of collocation points.")
-            raise ValueError(msg.format(self._col_points_min))
-        if points_max > 10:
-            msg = ("The maximum number of collocation points recommended in a single mesh sections is 10 due to the numerical instabilty of Lagrange polynomial interpolation above this threshold.")
-            raise ValueError(msg)
-        self._col_points_max = points_max
-
-    @property
-    def display_mesh_refinement_info(self):
-        return self._display_mesh_refinement_info
-
-    @display_mesh_refinement_info.setter
-    def display_mesh_refinement_info(self, val):
-        self._display_mesh_refinement_info = bool(val)
-
-    @property
-    def display_mesh_result_info(self):
-        return self._display_mesh_result_info
-
-    @display_mesh_result_info.setter
-    def display_mesh_result_info(self, val):
-        self._display_mesh_result_info = bool(val)
-
-    @property
-    def display_mesh_result_graph(self):
-        return self._display_mesh_result_graph
-
-    @display_mesh_result_graph.setter
-    def display_mesh_result_graph(self, val):
-        self._display_mesh_result_graph = bool(val)

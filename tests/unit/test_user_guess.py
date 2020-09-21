038a7c855da65dd89c29598bafc905ca65b047fc
Sam Brockie <sgb39@cam.ac.uk>
2020-09-21 11:00:36 +0100

    Add basic test for format checking of time guess

--- tests/unit/test_user_guess.py
+++ tests/unit/test_user_guess.py
@@ -1,24 +1,28 @@
-"""Tests for sanitising and processing user-supplied guesses.
-"""
+"""Tests for sanitising and processing user-supplied guesses."""
+
 import numpy as np
 import pytest
-import sympy as sym
-import sympy.physics.mechanics as me
 
 import pycollo
 
 
-# def test_create_guess():
-#     """Test whether a :obj:`PhaseGuess` can be instantiated sucessfully."""
-#     _ = pycollo.PhaseGuess()
-#     _ = pycollo.PhaseGuess()
+@pytest.mark.usefixtures("_ocp_without_guess_fixture")
+class TestUserGuess:
 
+    t0 = 0.0
+    tF = 10000.0
 
-# def test_1():
-#     """Dummy test 1."""
-#     assert(1 == 1)
+    @pytest.fixture(autouse=True)
+    def _ocp_without_guess_fixture(self):
+        """Simple fixture setting up an empty :obj:`OptimalControlProblem`."""
+        self.ocp = pycollo.OptimalControlProblem("Test OCP")
+        _ = self.ocp.new_phase(name="A")
 
-
-# def test_2():
-#     """Dummy test 2."""
-#     assert(1 != 2)
+    @pytest.mark.parametrize("test_input, expected",
+                             [([t0, tF], np.array([t0, tF])),
+                              ([t0, tF], np.array([t0, tF])),
+                              ])
+    def test_time_guess(self, test_input, expected):
+        self.ocp.phases.A.guess.time = test_input
+        assert type(self.ocp.phases.A.guess.time) == type(expected)
+        assert np.array_equal(self.ocp.phases.A.guess.time, expected)

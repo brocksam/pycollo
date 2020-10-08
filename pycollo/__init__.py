f59c8a138aea76d59f1d3a77c8e538b6be94faad
Sam Brockie <sgb39@cam.ac.uk>
2020-10-08 12:19:18 +0100

    Allow submodule import of Quadrature

--- pycollo/__init__.py
+++ pycollo/__init__.py
@@ -1,4 +1,10 @@
+"""Govern imports for Pycollo package."""
+
+# Explicitly available classes
 from .bounds import *
 from .guess import *
 from .settings import *
 from .optimal_control_problem import *
+
+# Modules accessible as submodules
+from . import quadrature

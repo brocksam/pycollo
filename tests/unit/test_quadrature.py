a68f7bb5234726974dd685afdf365b87598c9489
Sam Brockie <sgb39@cam.ac.uk>
2020-10-08 12:18:57 +0100

    Create new test modules

---
+++ tests/unit/test_quadrature.py
@@ -0,0 +1,8 @@
+"""Test for correct numerical calculation of quadrature.
+
+Quadrature schemes computed are: Radau (LGR), Lobatto (LGL) and Gauss (LG). 
+
+"""
+
+
+from pycollo.quadrature import Quadrature

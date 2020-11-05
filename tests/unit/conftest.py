5056333b3060ecd4e90a0f32ebfcfc7325e4467a
Sam Brockie <sgb39@cam.ac.uk>
2020-11-05 16:17:56 +0000

    Add fixture for brachistochrone OCP

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -13,6 +13,70 @@ FixtureData = namedtuple("FixtureData", ["ocp", "syms"])
 
 
 @pytest.fixture
+def brachistochrone_fixture():
+    """Fixture for the fully defined, but uninitialised, Brachistochrone OCP.
+
+    See the example `examples/brachistochrone.py` for a description of and
+    reference for this optimal control problem.
+
+    """
+
+    x, y, v, u = sym.symbols("x y v u")
+    Symbols = namedtuple("Symbols", ["x", "y", "v", "u"])
+    fixture_syms = Symbols(x, y, v, u)
+
+    g = 9.81
+    t0 = 0
+    tfmin = 0
+    tfmax = 10
+    x0 = 0
+    y0 = 0
+    v0 = 0
+    xf = 2
+    yf = 2
+    xmin = 0
+    xmax = 10
+    ymin = 0
+    ymax = 10
+    vmin = -50
+    vmax = 50
+    umin = -np.pi / 2
+    umax = np.pi / 2
+
+    problem = pycollo.OptimalControlProblem(name="Brachistochrone")
+    phase = problem.new_phase(name="A")
+    phase.state_variables = [x, y, v]
+    phase.control_variables = u
+    phase.state_equations = [v * sym.sin(u),
+                             v * sym.cos(u),
+                             g * sym.cos(u),
+                             ]
+    phase.auxiliary_data = {}
+    problem.objective_function = phase.final_time_variable
+
+    phase.bounds.initial_time = 0.0
+    phase.bounds.final_time = [tfmin, tfmax]
+    phase.bounds.state_variables = [[xmin, xmax], [ymin, ymax], [vmin, vmax]]
+    phase.bounds.control_variables = [[umin, umax]]
+    phase.bounds.initial_state_constraints = {x: x0, y: y0, v: v0}
+    phase.bounds.final_state_constraints = {x: xf, y: yf}
+
+    phase.guess.time = np.array([t0, tfmax])
+    phase.guess.state_variables = np.array([[x0, xf], [y0, yf], [v0, v0]])
+    phase.guess.control_variables = np.array([[0, umax]])
+
+    problem.settings.display_mesh_result_graph = True
+    problem.settings.derivative_level = 2
+    problem.settings.scaling_method = "bounds"
+    problem.settings.quadrature_method = "lobatto"
+    problem.settings.max_mesh_iterations = 10
+
+    fixture_data = FixtureData(problem, fixture_syms)
+
+    return fixture_data
+
+
+@pytest.fixture
 def double_pendulum_fixture():
     """Fixture for the fully defined, but uninitialised, double pendulum
     swing-up problem.

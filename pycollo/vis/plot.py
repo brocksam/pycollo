20f91d8b8768c484670b905ff642517445356326
Sam Brockie <sgb39@cam.ac.uk>
2020-11-05 11:53:41 +0000

    Add support for mesh density plotting

--- pycollo/vis/plot.py
+++ pycollo/vis/plot.py
@@ -2,26 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+Y = "y"
+DY = "dy"
+U = "u"
 POINT_OPTIONS = {"marker": "x",
                  "markersize": 5,
                  "linestyle": "",
                  "color": "black"}
 LINE_OPTIONS = {"linewidth": 2}
-TITLES = {"y": "States", "dy": "State Derivatives", "u": "Controls"}
+MESH_OPTIONS = {"align": "edge",
+                "alpha": 0.5}       
+X_TITLES = {"y": "States", "dy": "State Derivatives", "u": "Controls"}
 
 
-def plot_solution(solution, *, iterpolated=True, plot_y=True, plot_dy=True,
-                  plot_u=True):
+def plot_solution(solution, *, iterpolated=True, plot_y=True, plot_dy=False,
+                  plot_u=False, render=True):
     if plot_y:
-        t_data_phases, y_datas_phases = interpolate_x_solution(solution, "y")
-        render_x_solution(solution, "y", t_data_phases, y_datas_phases)
+        t_data_phases, y_datas_phases = interpolate_x_solution(solution, Y)
+        render_x_solution(solution, Y, t_data_phases, y_datas_phases)
     if plot_dy:
-        t_data_phases, dy_datas_phases = interpolate_x_solution(solution, "dy")
-        render_x_solution(solution, "dy", t_data_phases, dy_datas_phases)
+        t_data_phases, dy_datas_phases = interpolate_x_solution(solution, DY)
+        render_x_solution(solution, DY, t_data_phases, dy_datas_phases)
     if plot_u:
-        t_data_phases, u_datas_phases = interpolate_x_solution(solution, "u")
-        render_x_solution(solution, "u", t_data_phases, u_datas_phases)
-    plot_buffered_renders()
+        t_data_phases, u_datas_phases = interpolate_x_solution(solution, U)
+        render_x_solution(solution, U, t_data_phases, u_datas_phases)
+    plot_buffered_renders(render)
 
 
 def interpolate_x_solution(solution, x):
@@ -68,9 +73,34 @@ def render_x_solution(solution, x, t_data_phases, x_datas_phases):
             plt.plot(t_data, x_data, **LINE_OPTIONS, label=label)
     plt.legend(loc="upper left")
     plt.grid(True)
-    plt.title(TITLES[x])
+    plt.title(X_TITLES[x])
     plt.xlabel("Time / $s$")
+    plt.ylabel(f"{X_TITLES[x]} Values")
 
 
-def plot_buffered_renders():
-    plt.show()
+def plot_mesh(mesh, *, render=True):
+    try:
+        iter(mesh)
+    except (TypeError, ValueError):
+        mesh = [mesh]
+    for i, _ in enumerate(mesh[0].p):
+        plot_phase_mesh(mesh, i)
+    plot_buffered_renders(render)
+
+
+def plot_phase_mesh(meshes, i):
+    plt.figure()
+    for mesh in meshes:
+        xs = mesh.tau[i][mesh.mesh_index_boundaries[i][:-1]]
+        widths = mesh.h_K[i]
+        heights = mesh.N_K[i] / mesh.h_K[i]
+        plt.bar(x=xs, height=heights, width=widths, **MESH_OPTIONS)
+    plt.grid(True)
+    plt.title("Phase Mesh Density")
+    plt.xlabel("Tau")
+    plt.ylabel("Node Density / $node$")
+
+
+def plot_buffered_renders(render):
+    if render:
+        plt.show()

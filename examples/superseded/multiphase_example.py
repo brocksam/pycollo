1c6eef4d7945b728a1b10aa3a41fd937f7c76f60
Sam Brockie <sgb39@cam.ac.uk>
2020-11-23 12:03:59 +0000

    Adjust problem settings for debugging

--- examples/superseded/multiphase_example.py
+++ examples/superseded/multiphase_example.py
@@ -16,11 +16,12 @@ s1 = sym.Symbol('s1')
 a0 = sym.Symbol('a0')
 a1 = sym.Symbol('a1')
 
-circle_radius = 1.0
+circle_radius = 1.1
 
 problem = pycollo.OptimalControlProblem(
 	name="Multiphase example problem", 
-	parameter_variables=s0)
+	parameter_variables=s0
+	)
 
 phase_A = problem.new_phase(name="A")
 phase_A.state_variables = [y0, y1, y2, y3]
@@ -36,7 +37,7 @@ phase_A.integrand_functions = [u0**2, u1**2]
 phase_A.auxiliary_data = {}
 
 phase_A.bounds.initial_time = 0
-phase_A.bounds.final_time = 1
+phase_A.bounds.final_time = [0, 2]
 phase_A.bounds.state_variables = {
 	y0: [-3, 3],
 	y1: [-3, 3],
@@ -81,14 +82,14 @@ phase_B = problem.new_phase_like(
 	)
 phase_B.auxiliary_data = {}
 
-phase_B.bounds.initial_time = 1
+phase_B.bounds.initial_time = [0, 2]
 phase_B.bounds.final_time = 2
-phase_B.bounds.initial_state_constraints = {
-	y0: 0,
-	y1: 2,
-	y2: 0,
-	y3: 0,
-	}
+# phase_B.bounds.initial_state_constraints = {
+# 	y0: 0,
+# 	y1: 2,
+# 	y2: 0,
+# 	y3: 0,
+# 	}
 phase_B.bounds.final_state_constraints = {
 	y0: -1,
 	y1: -2,
@@ -108,7 +109,8 @@ phase_B.guess.integral_variables = np.array([0, 0])
 problem.objective_function = (phase_A.integral_variables[0] 
 	+ phase_A.integral_variables[1]
 	+ phase_B.integral_variables[0]
-	+ phase_B.integral_variables[1])
+	+ phase_B.integral_variables[1]
+)
 
 problem.auxiliary_data = {
 	a0: u0 / s0,
@@ -116,6 +118,7 @@ problem.auxiliary_data = {
 }
 
 problem.endpoint_constraints = [
+	phase_A.final_time_variable - phase_B.initial_time_variable,
 	phase_A.final_state_variables.y0 - phase_B.initial_state_variables.y0,
 	phase_A.final_state_variables.y1 - phase_B.initial_state_variables.y1,
 	phase_A.final_state_variables.y2 - phase_B.initial_state_variables.y2,
@@ -128,31 +131,32 @@ problem.bounds.endpoint_constraints = [
 	0,
 	0,
 	0,
+	0,
 	]
 
 problem.guess.parameter_variables = np.array([1.5])
 
-problem.settings.nlp_tolerance = 1e-8
-problem.settings.mesh_tolerance = 1e-7
-problem.settings.maximise_objective = False
-problem.settings.backend = "casadi"
-problem.settings.scaling_method = "none"
-problem.settings.assume_inf_bounds = False
-problem.settings.inf_value = 1e16
+# problem.settings.nlp_tolerance = 1e-8
+# problem.settings.mesh_tolerance = 1e-7
+# problem.settings.maximise_objective = False
+# problem.settings.backend = "casadi"
+problem.settings.scaling_method = "bounds"
+# problem.settings.assume_inf_bounds = False
+# problem.settings.inf_value = 1e16
 problem.settings.check_nlp_functions = False
 # problem.settings.dump_nlp_check_json = "pycollo"
 # problem.settings.collocation_points_min = 2
 # problem.settings.collocation_points_max = 8
-problem.settings.derivative_level = 1
-problem.settings.max_mesh_iterations = 10
-problem.settings.display_mesh_result_graph = False
+# problem.settings.derivative_level = 2
+# problem.settings.max_mesh_iterations = 10
+problem.settings.display_mesh_result_graph = True
 
-# phase_A.mesh.number_mesh_sections = 2
-# phase_A.mesh.mesh_section_sizes = [1/2, 1/2]
-# phase_A.mesh.number_mesh_section_nodes = [2, 3]
-# phase_B.mesh.number_mesh_sections = 2
-# phase_B.mesh.mesh_section_sizes = [1/2, 1/2]
-# phase_B.mesh.number_mesh_section_nodes = [3, 2]
+# phase_A.mesh.number_mesh_sections = 1
+# phase_A.mesh.mesh_section_sizes = [1]
+# phase_A.mesh.number_mesh_section_nodes = [2]
+# phase_B.mesh.number_mesh_sections = 1
+# phase_B.mesh.mesh_section_sizes = [1]
+# phase_B.mesh.number_mesh_section_nodes = [2]
 
 problem.initialise()
 problem.solve()

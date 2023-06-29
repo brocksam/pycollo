0958496a80bd4797e34d89869b9b560d0ccd1c90
Sam Brockie <sambrockie@icloud.com>
2023-06-29 16:03:45 +0200

    Rename to "python"

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ intersphinx_mapping = {
     "numpy": ("https://numpy.org/doc/stable/", None),
     "scipy": ("https://docs.scipy.org/doc/scipy/", None),
     "sympy": ("https://docs.sympy.org/latest/", None),
-    "python3": ("http://docs.python.org/3", None),
+    "python": ("http://docs.python.org/3", None),
 }
 
 
